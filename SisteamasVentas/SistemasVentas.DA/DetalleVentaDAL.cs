eca24818b260d2194f8a31f432613082be8f4eaf
Rocío <tj.rocio.cruz.z@upds.net.bo>
2024-03-26 10:22:22 -0400

    proyecto

---
+++ SisteamasVentas/SistemasVentas.DA/DetalleVentaDAL.cs
@@ -0,0 +1,62 @@
+﻿using SistemasVentas.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class DetalleVentaDAL
+    {
+        public DataTable ListarDetalleVentaDal()
+        {
+            string consulta = "select * from detalleventa";
+            DataTable Lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return Lista;
+        }
+        public void InsertarDetalleVentaDAL(DetalleVenta detalleventa)
+        {
+            string consulta = "insert into detalleventa values(" + detalleventa.IdVenta + "," +
+                                                          "" + detalleventa.IdProducto + "," +
+                                                          "'" + detalleventa.Cantidad + "'," +
+                                                          "'" + detalleventa.PrecioVenta + "'," +
+                                                          "'" + detalleventa.SubTotal + "'," +
+                                                          "'Exitoso')";
+            conexion.Ejecutar(consulta);
+        }
+        DetalleVenta p = new DetalleVenta();
+        public DetalleVenta ObtenerDetalleVentaIdDal(int id)
+        {
+            string consulta = "select * from detalleventa where iddetalleventa=" + id;
+            DataTable tabla = conexion.EjecutarDataTabla(consulta, "asdas");
+            if (tabla.Rows.Count > 0)
+            {
+                p.IdDetaleVenta = Convert.ToInt32(tabla.Rows[0]["iddetalleventa"]);
+                p.IdVenta = Convert.ToInt32(tabla.Rows[0]["idventa"]);
+                p.IdProducto = Convert.ToInt32(tabla.Rows[0]["idproducto"]);
+                p.Cantidad = Convert.ToInt32(tabla.Rows[0]["cantidad"]);
+                p.PrecioVenta = Convert.ToDecimal(tabla.Rows[0]["precioventa"]);
+                p.SubTotal = Convert.ToDecimal(tabla.Rows[0]["subtotal"]);
+                p.Estado = tabla.Rows[0]["estado"].ToString();
+            }
+            return p;
+        }
+        public void EditarDetalleVentaDal(DetalleVenta p)
+        {
+            string consulta = "update detalleventa set idventa=" + p.IdVenta + "," +
+                                                        "idproducto=" + p.IdProducto + "," +
+                                                        "cantidad=" + p.Cantidad + "," +
+                                                        "precioventa=" + p.PrecioVenta + "," +
+                                                        "subtotal=" + p.SubTotal + " " +
+                                                "where iddetalleventa=" + p.IdDetaleVenta;
+            conexion.Ejecutar(consulta);
+        }
+        public void EliminarDetalleVentaDal(int id)
+        {
+            string consulta = "delete from detalleventa where iddetalleventa=" + id;
+            conexion.Ejecutar(consulta);
+        }
+    }
+}
